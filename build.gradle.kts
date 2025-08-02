619dddcf0b40045a8f9424852e083661f883bb60
ybkim-dev <beanybigggy@gmail.com>
2025-08-02 15:43:19 +0900

    chore : test 환경 build.gradle import

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,19 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
+
+
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+	testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+	testImplementation("io.kotest:kotest-property:5.9.1")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.mockk:mockk:1.14.5")
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 kotlin {
